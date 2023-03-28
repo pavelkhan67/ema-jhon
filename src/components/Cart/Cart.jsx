31420593facb006a8e327649a069cd53899410f5
Pavel Khan <pvelkhantopu@gmail.com>
2023-03-28 21:14:59 +0600

    localstorage part done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,12 +5,15 @@ const Cart = (props) => {
     const {cart} = props;
 
     console.log(cart);
-
+    let quantity = 0;
     let total = 0;
     let shipping = 0;
     for(const product of cart){
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+
     }
 
     const tax = total * 7 / 100; 
@@ -20,7 +23,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
