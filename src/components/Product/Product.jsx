d5ffb9651e9b8605e58b170c9681f197b05ab4f2
Pavel Khan <pvelkhantopu@gmail.com>
2023-03-28 15:30:09 +0600

    second commit

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {img, name, price, seller, ratings } = props.product
+    return (
+        <div className='product'>
+            <div style={{padding:'10px'}}>
+            <img src={img} alt="" />
+            </div>
+            <div style={{padding:'14px', marginBottom:'20px'}}>
+            <h4 className='p-name'>{name}</h4>
+            <p style={{marginTop:'5px'}}>Price: ${price}</p>
+            <p style={{marginBottom:'0px'}}><small>Manufacturer: {seller}</small></p>
+            <p style={{marginTop:'0px'}}><small>Ratting: {ratings} star</small></p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
